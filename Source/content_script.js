31aeaf91a7406105846974e34bfb28e0ef86f478
JBirdVegas <jbirdvegas@gmail.com>
2013-03-14 17:46:26 -0400

    Make searches target Apple -> The Fruit Cult

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,42 +1,47 @@
 walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+function walk(node) {
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+    var child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    switch (node.nodeType) {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+    var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+    // Apple
+    var n = v.match(/Apple/i);
+    var m = v.match(/apple/i);
+    if (n || m) {
+        this.handled = true;
+    }
+    v = v.replace(/\bApple\b/g, "The Fruit Cult");
+    v = v.replace(/\bapple\b/g, "the fruit cult");
+
+    textNode.nodeValue = v;
 }
 
-
+var handled;
+window.onload = function () {
+    if (this.handled) {
+        confirm("So... are you apple's bitch yet?");
+    }
+}
